b665c57b31a0adbf86ceb85447a6989a7ec62b7d
Victor Giles <victorgiles02@gmail.com>
2022-08-20 14:22:58 -0300

    primeiro commit

---
+++ src/main/java/com/victor/funilaria/FunilariaApplication.java
@@ -0,0 +1,13 @@
+package com.victor.funilaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunilariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunilariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/funilaria/FunilariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.funilaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunilariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
